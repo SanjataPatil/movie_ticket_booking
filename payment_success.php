f7d29892152eee114b7ce93db5f43f9bb77c81d7
SanjataPatil <131331864+SanjataPatil@users.noreply.github.com>
2025-05-27 22:07:42 +0530

    Add files via upload

---
+++ payment_success.php
@@ -0,0 +1,5 @@
+<?php
+session_start();
+echo "<h1 style='text-align:center;'>Payment Successful!</h1>";
+echo "<p style='text-align:center;'>Thank you,  Your movie ticket has been booked.</p>";
+?>
